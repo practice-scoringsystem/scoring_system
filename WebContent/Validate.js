116b7ea90a19dd9d1eb422d0841926079a24d76d
hayashi-3 <hayashi.gml@gmail.com>
2021-03-11 15:55:39 +0900

    [fix]is_user_register

--- WebContent/Validate.js
+++ WebContent/Validate.js
@@ -21,6 +21,9 @@ function check() {
 	} else if (!form.confirmPassword.value.match(/^([a-zA-Z0-9]{8,})$/)) {
 		alert("パスワードは半角英数字で8文字以上に設定してください");
 		return false;
+	} else if (form.password.value != form.confirmPassword.value){
+      alert("パスワードと確認用パスワードが一致しません"); // 一致していなかったら、エラーメッセージを表示する
+      return false;
 	} else {
 		//条件に一致しない場合(入力されている場合)
 		return true; //送信ボタン本来の動作を実行します
